e478cde5ac60149606ff297d9dc26cc233c7257b
Alexander O'Mara <me@alexomara.com>
2020-03-14 22:46:10 -0400

    Implemented command argument parsing

--- src/shell.cc
+++ src/shell.cc
@@ -11,7 +11,10 @@ Shell::Shell(std::string host, std::string welcome) {
 }
 
 void Shell::main() {
-	std::cout << this->welcome << std::endl;
+	// Print welcome message if one set.
+	if (this->welcome.length()) {
+		std::cout << this->welcome << std::endl;
+	}
 
 	Replxx rx;
 	rx.install_window_change_handler();
@@ -20,7 +23,7 @@ void Shell::main() {
 
 	std::string prompt = "\x1b[1;32mhacker@" + this->host + "\x1b[0m$ ";
 	for (;;) {
-		char const * cinput{ nullptr };
+		char const * cinput = nullptr;
 		do {
 			cinput = rx.input(prompt);
 		}
@@ -28,14 +31,293 @@ void Shell::main() {
 		if (cinput == nullptr) {
 			break;
 		}
+		std::string input = cinput;
 
-		std::string input {cinput};
-		if (input.empty()) {
+		// Parse arguments.
+		std::vector<std::string> args;
+		try {
+			args = this->parseArgs(input);
+		}
+		catch (std::invalid_argument ex) {
+			rx.history_add(input);
+			std::cout << "Shell Error: " << ex.what() << std::endl;
+			continue;
+		}
+
+		// If no arguments after parsing, ignore.
+		if (!args.size()) {
 			continue;
 		}
 		rx.history_add(input);
 
-		// TODO: Parse into argc/argv.
-		std::cout << input << std::endl;
+		// Run command if possible.
+		if (!this->runCommand(args)) {
+			std::cout <<
+				"Shell Error: Command not found: \"" <<
+				args[0] <<
+				"\"" <<
+				std::endl;
+		}
 	}
+}
+
+bool Shell::runCommand(const std::vector<std::string> & args) {
+	auto argsArgv = this->argsArgvView(args);
+	int argc = argsArgv.size();
+	char ** argv = argsArgv.data();
+
+	// TODO: Use to run command.
+	std::cout << "[" << argc << "]:\n";
+	for (int i = 0; i < argc; i++) {
+		std::cout << "  [" << i << "]: " << argv[i] << "\n";
+	}
+
+	return false;
+}
+
+std::string Shell::shellUnquote(std::string quotedString) {
+	// Function based on glib gshell g_shell_unquote.
+	std::string result = "";
+	size_t end = 0;
+	size_t start = 0;
+	size_t l = quotedString.length();
+
+	while (start < l) {
+		while (
+			start < l &&
+			!(
+				quotedString[start] == '"' ||
+				quotedString[start] == '\''
+			)
+		) {
+			if (quotedString[start] == '\\') {
+				start++;
+
+				if (start < l) {
+					if (quotedString[start] != '\n') {
+						result += quotedString[start];
+					}
+					start++;
+				}
+			}
+			else {
+				result += quotedString[start];
+				start++;
+			}
+		}
+
+		if (start < l) {
+			std::string dest = "";
+			int breakout = 0;
+			do {
+				size_t s = start;
+				char quoteChar = quotedString[s];
+				if (quoteChar != '"' && quoteChar != '\'') {
+					throw std::invalid_argument("Bad Quoting");
+				}
+				s++;
+
+				if (quoteChar == '"') {
+					while (s < l) {
+						if (quotedString[s] == '"') {
+							s++;
+							end = s;
+
+							breakout = 1;
+							break;
+						}
+						else if (quotedString[s] == '\\') {
+							s++;
+							if (
+								quotedString[s] == '"' ||
+								quotedString[s] == '\\' ||
+								quotedString[s] == '`' ||
+								quotedString[s] == '$' ||
+								quotedString[s] == '\n'
+							) {
+								dest += quotedString[s];
+								s++;
+							}
+							else {
+								dest += '\\';
+							}
+						}
+						else {
+							dest += quotedString[s];
+							s++;
+						}
+					}
+					if (breakout) {
+						break;
+					}
+				}
+				else {
+					while (s < l) {
+						if (quotedString[s] == '\'') {
+							s++;
+							end = s;
+
+							breakout = 1;
+							break;
+						}
+						else {
+							dest += quotedString[s];
+							s++;
+						}
+					}
+					if (breakout) {
+						break;
+					}
+				}
+			}
+			while (0);
+
+			if (breakout) {
+				result += dest;
+				start = end;
+			}
+			else {
+				throw std::invalid_argument("Bad Quoting");
+			}
+		}
+	}
+
+	return result;
+}
+
+std::vector<std::string> Shell::tokenizeCommandLine(std::string commandLine) {
+	// Function based on glib gshell tokenize_command_line.
+	std::vector<std::string> result;
+	char currentQuote = '\0';
+	std::string currentToken = "";
+	std::string * currentTokenP = nullptr;
+	bool quoted = false;
+
+	size_t l = commandLine.length();
+	size_t i = 0;
+	while (i < l) {
+		char p = commandLine[i];
+
+		if (currentQuote == '\\') {
+			if (p != '\n') {
+				if (!currentTokenP) {
+					currentToken = "";
+					currentTokenP = &currentToken;
+				}
+				*currentTokenP += '\\';
+				*currentTokenP += p;
+			}
+			currentQuote = '\0';
+		}
+		else if (currentQuote == '#') {
+			while (i < l && commandLine[i] != '\n') {
+				i++;
+			}
+
+			if (i < l) {
+				p = commandLine[i];
+			}
+			currentQuote = '\0';
+
+			if (i >= l) {
+				break;
+			}
+		}
+		else if (currentQuote) {
+			if (
+				p == currentQuote &&
+				!(currentQuote == '"' && quoted)
+			) {
+				currentQuote = '\0';
+			}
+			if (!currentTokenP) {
+				currentToken = "";
+				currentTokenP = &currentToken;
+			}
+			*currentTokenP += p;
+		}
+		else {
+			if (p == '\n' || p == ' ' || p == '\t') {
+				if (currentTokenP) {
+					result.push_back(*currentTokenP);
+					currentTokenP = nullptr;
+				}
+			}
+			else if (p == '\'' || p == '"' || p == '\\') {
+				if (p != '\\') {
+					if (!currentTokenP) {
+						currentToken = "";
+						currentTokenP = &currentToken;
+					}
+					*currentTokenP += p;
+				}
+				currentQuote = p;
+			}
+			else if (p == '#') {
+				if (!i) {
+					currentQuote = p;
+				}
+				else {
+					if (
+						commandLine[i - 1] == ' ' ||
+						commandLine[i - 1] == '\n' ||
+						commandLine[i - 1] == '\0'
+					) {
+						currentQuote = p;
+					}
+					else {
+						if (!currentTokenP) {
+							currentToken = "";
+							currentTokenP = &currentToken;
+						}
+						*currentTokenP += p;
+					}
+				}
+			}
+			else {
+				if (!currentTokenP) {
+					currentToken = "";
+					currentTokenP = &currentToken;
+				}
+				*currentTokenP += p;
+			}
+		}
+
+		if (p != '\\') {
+			quoted = false;
+		}
+		else {
+			quoted = !quoted;
+		}
+
+		i++;
+	}
+
+	if (currentTokenP) {
+		result.push_back(*currentTokenP);
+		currentTokenP = nullptr;
+	}
+
+	if (currentQuote != '\0') {
+		throw std::invalid_argument("Bad Quoting");
+	}
+
+	return result;
+}
+
+std::vector<std::string> Shell::parseArgs(std::string commandLine) {
+	// Function based on glib gshell g_shell_parse_argv.
+	std::vector<std::string> args = this->tokenizeCommandLine(commandLine);
+	for (size_t i = 0, l = args.size(); i < l; i++) {
+		args[i] = this->shellUnquote(args[i]);
+	}
+	return args;
+}
+
+std::vector<char *> Shell::argsArgvView(const std::vector<std::string> & args) {
+	std::vector<char *> argc;
+	for (size_t i = 0, l = args.size(); i < l; i++) {
+		argc.push_back((char *)args.at(i).c_str());
+	}
+	return argc;
 }
--- src/shell.hh
+++ src/shell.hh
@@ -9,4 +9,36 @@ class Shell {
 
 		Shell(std::string host, std::string welcome);
 		void main();
+
+		bool runCommand(const std::vector<std::string> & args);
+
+		/**
+		 * Unquote a shell argument.
+		 *
+		 * @param quotedString Quoted string.
+		 * @returns Unquoted string.
+		 */
+		std::string shellUnquote(std::string quotedString);
+
+		/**
+		 * Tokenize a command line string into vector.
+		 *
+		 * @param commandLine Command line string.
+		 */
+		std::vector<std::string> tokenizeCommandLine(std::string commandLine);
+
+		/**
+		 * Parse a command line string into vector.
+		 *
+		 * @param commandLine Command line string.
+		 */
+		std::vector<std::string> parseArgs(std::string commandLine);
+
+		/**
+		 * Get argv vector view of args.
+		 *
+		 * @param args Arguments vector
+		 * @returns Pointer vector.
+		 */
+		std::vector<char *> argsArgvView(const std::vector<std::string> & args);
 };
