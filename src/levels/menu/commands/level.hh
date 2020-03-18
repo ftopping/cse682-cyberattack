1d1d8ed59a71f156843d3ef12013f3d0dfb76517
Alexander O'Mara <me@alexomara.com>
2020-03-18 19:00:52 -0400

    Level command documentation comments

--- src/levels/menu/commands/level.hh
+++ src/levels/menu/commands/level.hh
@@ -5,11 +5,36 @@
 
 class LevelMenuCommandLevel: public Command {
 	public:
+		/**
+		 * Constructor.
+		 *
+		 * @param menu Menu instance.
+		 * @param level Level number.
+		 */
 		LevelMenuCommandLevel(LevelMenu * menu, int level);
+
+		/**
+		 * Destructor.
+		 */
 		virtual ~LevelMenuCommandLevel();
-		int main(int argc, char ** argv);
+
+		/**
+		 * The main entry point.
+		 *
+		 * @param argc Argument count.
+		 * @param argv Argument values.
+		 * @returns Exit code.
+		 */
+		virtual int main(int argc, char ** argv);
 
 	protected:
+		/**
+		 * Menu instance.
+		 */
 		LevelMenu * menu;
+
+		/**
+		 * Level number.
+		 */
 		int level;
 };
