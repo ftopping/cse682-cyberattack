a4abfdbd7b6f45f3258d19a4a96992f8c09719c1
Alexander O'Mara <me@alexomara.com>
2020-03-18 18:50:01 -0400

    Game documentation comments

--- src/game.hh
+++ src/game.hh
@@ -6,13 +6,34 @@
 
 class Game {
 	public:
+		/**
+		 * Flag to start a level instead of menu.
+		 */
 		int startLevel = 0;
+
+		/**
+		 * Current level of completion.
+		 */
 		int finishedLevel = 0;
 
+		/**
+		 * Constructor.
+		 */
 		Game();
+
+		/**
+		 * Destructor.
+		 */
 		~Game();
+
+		/**
+		 * Main loop.
+		 */
 		void main();
 
 	protected:
+		/**
+		 * Level instances.
+		 */
 		std::vector<Level *> levels;
 };
