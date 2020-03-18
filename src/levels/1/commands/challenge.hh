de5cd1f26e575e8af07d3ae079fd64433a12c45a
Alexander O'Mara <me@alexomara.com>
2020-03-18 19:03:08 -0400

    Level 1 commands documentation comments

--- src/levels/1/commands/challenge.hh
+++ src/levels/1/commands/challenge.hh
@@ -5,10 +5,30 @@
 
 class Level1CommandChallenge: public Command {
 	public:
+		/**
+		 * Constructor.
+		 *
+		 * @param level Level instance.
+		 */
 		Level1CommandChallenge(Level1 * level);
+
+		/**
+		 * Destructor.
+		 */
 		virtual ~Level1CommandChallenge();
-		int main(int argc, char ** argv);
+
+		/**
+		 * The main entry point.
+		 *
+		 * @param argc Argument count.
+		 * @param argv Argument values.
+		 * @returns Exit code.
+		 */
+		virtual int main(int argc, char ** argv);
 
 	protected:
+		/**
+		 * Level instance.
+		 */
 		Level1 * level;
 };
--- src/levels/1/commands/learn.hh
+++ src/levels/1/commands/learn.hh
@@ -4,7 +4,22 @@
 
 class Level1CommandLearn: public Command {
 	public:
+		/**
+		 * Constructor.
+		 */
 		Level1CommandLearn();
+
+		/**
+		 * Destructor.
+		 */
 		virtual ~Level1CommandLearn();
-		int main(int argc, char ** argv);
+
+		/**
+		 * The main entry point.
+		 *
+		 * @param argc Argument count.
+		 * @param argv Argument values.
+		 * @returns Exit code.
+		 */
+		virtual int main(int argc, char ** argv);
 };
