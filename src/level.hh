5eaa18e3bc88e813b79f1bd56258bb597e84c6a8
Alexander O'Mara <me@alexomara.com>
2020-03-18 18:48:36 -0400

    Level documentation comments

--- src/level.hh
+++ src/level.hh
@@ -8,15 +8,48 @@
 
 class Level {
 	public:
+		/**
+		 * Flag for if challenge is passed.
+		 */
 		bool won = false;
 
+		/**
+		 * Constructor.
+		 */
 		Level();
+
+		/**
+		 * Destructor.
+		 */
 		virtual ~Level();
+
+		/**
+		 * Get welcome text for the shell.
+		 *
+		 * @returns Welcome text.
+		 */
 		virtual std::string welcome();
+
+		/**
+		 * Run level.
+		 *
+		 * @returns Returns true if challenge is beaten.
+		 */
 		virtual bool main();
 
 	protected:
+		/**
+		 * Level name.
+		 */
 		std::string name;
+
+		/**
+		 * Shell commands.
+		 */
 		std::vector<Command *> commands;
+
+		/**
+		 * Shell instance.
+		 */
 		Shell * shell = nullptr;
 };
