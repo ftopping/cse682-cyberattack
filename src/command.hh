19b148e3e93af684faffe858d32c4f742dd8647f
Alexander O'Mara <me@alexomara.com>
2020-03-18 18:51:08 -0400

    Command documentation comments

--- src/command.hh
+++ src/command.hh
@@ -5,10 +5,32 @@
 
 class Command {
 	public:
+		/**
+		 * Command name for shell.
+		 */
 		std::string name;
 
+		/**
+		 * Constructor.
+		 */
 		Command();
+
+		/**
+		 * Destructor.
+		 */
 		virtual ~Command();
+
+		/**
+		 * Initialization method.
+		 */
 		virtual void start();
+
+		/**
+		 * The main entry point.
+		 *
+		 * @param argc Argument count.
+		 * @param argv Argument values.
+		 * @returns Exit code.
+		 */
 		virtual int main(int argc, char ** argv);
 };
