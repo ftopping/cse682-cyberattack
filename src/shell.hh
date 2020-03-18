e6996c5530b148f6017f33737d2bacfb227d3d4f
Alexander O'Mara <me@alexomara.com>
2020-03-18 18:55:00 -0400

    More Shell documentation comments

--- src/shell.hh
+++ src/shell.hh
@@ -9,22 +9,72 @@
 
 class Shell {
 	public:
+		/**
+		 * Flage for the shell to exit.
+		 */
 		bool exit = false;
 
+		/**
+		 * Constructor.
+		 *
+		 * @param host Shell prompt host.
+		 * @param welcome Shell welcome text.
+		 */
 		Shell(std::string host, std::string welcome);
+
+		/**
+		 * Destructor.
+		 */
 		~Shell();
+
+		/**
+		 * Register command with shell.
+		 *
+		 * @param command Command instance.
+		 */
 		void addCommand(Command * command);
+
+		/**
+		 * Main loop.
+		 */
 		void main();
 
 	protected:
+		/**
+		 * Shell prompt host.
+		 */
 		std::string host;
+
+		/**
+		 * Shell welcome text.
+		 */
 		std::string welcome;
+
+		/**
+		 * Shell command name map.
+		 */
 		std::unordered_map<std::string, Command *> commandMap;
 
+		/**
+		 * Output the help text.
+		 */
 		void help();
 
+		/**
+		 * Get command by name if exists.
+		 *
+		 * @param name Command name.
+		 * @returns Command or null pointer.
+		 */
 		Command * getCommand(std::string name);
 
+		/**
+		 * Runn command with arguments.
+		 *
+		 * @param command Command instance.
+		 * @param args Shell arguments.
+		 * @returns Exit code.
+		 */
 		int runCommand(
 			Command * command,
 			const std::vector<std::string> & args
