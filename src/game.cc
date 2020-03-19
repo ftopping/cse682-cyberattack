ebaa004194af3604657ef7a6e9f59a1892f3f52c
ftopping <61859283+ftopping@users.noreply.github.com>
2020-03-19 14:14:03 -0600

    Update game.cc

--- src/game.cc
+++ src/game.cc
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <shlobj.h>
 
 #include "game.hh"
 
@@ -26,7 +29,31 @@ Game::~Game() {
 void Game::main() {
 	for (int startLevel = -1; this->startLevel || startLevel;) {
 		startLevel = this->startLevel;
-		this->startLevel = 0;
+		
+		// Searches for the current user documents library and appends our saveFile.txt to the end for
+		// the filepath string used later.
+    	// Requires windows.h & shlobj.h
+    	CHAR my_documents[MAX_PATH];
+    	HRESULT result = SHGetFolderPathA(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, my_documents);
+    	std::string filepath; 
+   		filepath += my_documents;
+    	filepath += "\\saveFile.txt";
+
+		// This checks if a file exists. If it does it pulls the value and stores in startLevel. 
+		// If it doesn't we set startLevel to 0 and continue.
+		// Requires fstream
+		struct stat buf;
+   		if (stat(filepath.c_str(), &buf) != -1) {
+        	std::ifstream saveFile;
+        	saveFile.open(filepath);
+			int level;
+        	saveFile >> level;
+			this->startLevel = level;
+        	saveFile.close();
+    	}
+    	else {
+        	this->startLevel = 0;
+    	}
 
 		Level * level = this->levels.at(startLevel);
 		bool won = level->main();
